caab03ba41ce54eb02aa74ef1e165933429db633
zhudongzhuo <zdzserver@163.com>
2020-05-21 14:59:14 +0800

    上传MD5方法

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/zdz1715/go-tools
+
+go 1.14
---
+++ hash/md5.go
@@ -0,0 +1,29 @@
+package hash
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+)
+
+func FileMd5(filename string) (string, error) {
+	file, err := os.Open(filename)
+	if err != nli {
+		return "", errors.New(fmt.Sprintf("open error: %v", err))
+	}
+	h := md5.New()
+	_, err = io.Copy(h, file)
+	if err != nil {
+		return "", errors.New(fmt.Sprintf("copy error: %v", err))
+	}
+	return hex.EncodeToString(h.Sum(nil)), nil
+}
+
+func StringMd5(str string) string {
+	md5New := md5.New()
+	md5New.Write([]byte(str))
+	return hex.EncodeToString(md5New.Sum(nil))
+}
